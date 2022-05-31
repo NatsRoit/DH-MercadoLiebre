12c2f0ebc689a2a36cb90f30deadc06f9b0ef1db
NatsRoit <nats.roitman@gmail.com>
2022-05-31 09:40:05 +0200

    M04C06 Ejercitación Formularios

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ const path = require('path');
 
 const app = express();
 
+app.use( express.static(path.resolve(__dirname, './public')));
+app.use( express.static(path.resolve(__dirname, './views')));
 
-const publicPath = path.join(__dirname, './public');
-app.use(express.static(publicPath))
+app.listen(3000, () => console.log("Server running on port 3000"));
 
-app.listen(3000, ()=>{
-    console.log('Servidor corriendo en el puerto 3000');
-});
-
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "/views/home.html"))
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
