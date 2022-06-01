5473d6431bb428279f936e0a88e84454a5751480
NatsRoit <nats.roitman@gmail.com>
2022-06-01 18:24:34 +0200

    M04C07 - Ejercitación Final

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const path = require('path');
 const app = express();
 
 app.use( express.static(path.resolve(__dirname, './public')));
-app.use( express.static(path.resolve(__dirname, './views')));
 
 app.listen(3000, () => console.log("Server running on port 3000"));
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
 });
