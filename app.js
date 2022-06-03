13290480cd5cc8af8d0d7f003fcf0af8c051e23e
NatsRoit <nats.roitman@gmail.com>
2022-06-03 02:11:30 +0200

    Pongo el listen al final en app.js

--- app.js
+++ app.js
@@ -5,11 +5,6 @@ const app = express();
 
 app.use( express.static(path.resolve(__dirname, './public')));
 
-app.listen(process.env.PORT || 3000, () => {
-        console.log("Server running on port 3000");
-    }
-);
-
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
@@ -20,3 +15,9 @@ app.get('/register', (req,res) => {
 app.get('/login', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'))
 });
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server running on port 3000");
+}
+);
+
